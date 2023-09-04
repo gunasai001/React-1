a1fbe4855253af74a3d782e68910b0c1fb5a0229
Guna Sai <saiguna2004@gmail.com>
2023-09-04 13:41:06 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
+<<<<<<< HEAD
 # React-1
 Basic project1 
+=======
+#### React Router Fix
+
+(Fix)[https://dev.to/dance2die/page-not-found-on-netlify-with-react-router-58mc]
+
+#### CRA Fix
+
+```
+
+"build": "CI= react-scripts build",
+
+```
+
+```sh
+npm install react-router-dom@6
+```
+>>>>>>> d72e182 (initial commit)
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolorem dolores hic quae animi ex fugit enim cumque, ipsam velit corporis fuga fugiat explicabo quia temporibus quod debitis saepe libero itaque earum dolore beatae distinctio. Modi enim quidem accusamus earum libero, quibusdam, quos hic numquam provident accusantium laborum, nisi assumenda!</p>
+    </section>
+  )
+}
+
+export default About
